edae05b1d2e3578446c04b78a9efa95e71e8aa17
AWeidenkopf <oliveira.anacalado@gmail.com>
2022-10-29 16:31:51 -0400

    initialize bartab file

---
+++ bar_tab.py
@@ -0,0 +1,15 @@
+from tkinter import Menu
+
+
+class Tab:
+
+  menu = {
+    'wine': 10,
+    'beer': 7,
+    'soda': 3,
+    'chicken': 19,
+    'steak': 20,
+    'pasta': 15,
+    'lava-cake': 6
+  }
+
